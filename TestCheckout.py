d0e973fb11a5da9887d3f79d6ad59ddbfd3f30c7
Ehsan Jahanara <ehsan.jnra@gmail.com>
2025-05-14 14:23:19 +0330

    duplicate test functions removed

--- TestCheckout.py
+++ TestCheckout.py
@@ -7,12 +7,6 @@ def checkout():
     checkout = Checkout()
     return checkout
 
-def test_canAddItemPrice(checkout):
-    checkout.addItemPrice("a",1)
-
-def test_canAddItem(checkout):
-    checkout.addItem("a")
-
 def test_canCalculateTotal(checkout):
     checkout.addItemPrice("a",1)
     checkout.addItem("a")
