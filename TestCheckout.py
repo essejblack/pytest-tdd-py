a4a996c0f3ebb475eb8f087e0936f555ed79e040
Ehsan Jahanara <ehsan.jnra@gmail.com>
2025-05-14 14:22:42 +0330

    item can be added with price

--- TestCheckout.py
+++ TestCheckout.py
@@ -2,6 +2,14 @@ import pytest
 
 from Checkout import Checkout
 
-def test_canAddItemPrice():
-    co = Checkout()
-    co.addItemPrice("a",1)
+@pytest.fixture()
+def checkout():
+    checkout = Checkout()
+    return checkout
+
+def test_canAddItemPrice(checkout):
+    checkout.addItemPrice("a",1)
+
+def test_canAddItem(checkout):
+    checkout.addItem("a")
+
