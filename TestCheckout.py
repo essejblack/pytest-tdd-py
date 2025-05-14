954f26a4bba5e318e5e7d0dcb02579dc6dc9459c
Ehsan Jahanara <ehsan.jnra@gmail.com>
2025-05-14 14:11:25 +0330

    clean unnecessary function

--- TestCheckout.py
+++ TestCheckout.py
@@ -2,10 +2,6 @@ import pytest
 
 from Checkout import Checkout
 
-
-def test_CanInitiateCheckout():
-    co = Checkout()
-
 def test_canAddItemPrice():
     co = Checkout()
     co.addItemPrice("a",1)
