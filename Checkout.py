d5c02b25f4e539ce88fe73a91c7be703a358ccbb
Ehsan Jahanara <ehsan.jnra@gmail.com>
2025-05-19 01:27:42 +0330

    calculations and discount added to the function

--- Checkout.py
+++ Checkout.py
@@ -1,11 +1,41 @@
 class Checkout:
+    class Discount:
+        def __init__(self, numberItems, price):
+            self.numberItems = numberItems
+            self.price = price
+
     def __init__(self):
         self.items = {}
         self.prices = {}
-        self.total = 0
-    def addItemPrice(self,item,price):
+        self.discounts = {}
+
+    def addItemPrice(self, item, price):
         self.prices[item] = price
-    def addItem(self,item):
-        self.total += self.prices[item]
+
+    def addItem(self, item):
+        if item not in self.prices:
+            raise Exception("Bad Item")
+        if item in self.items:
+            self.items[item] += 1
+        else:
+            self.items[item] = 1
+
     def calculateTotal(self):
-        return self.total
+        total = 0
+        for item, cnt in self.items.items():
+            if item in self.discounts:
+                discount = self.discounts[item]
+                if cnt >= discount.numberItems:
+                    numb = cnt / discount.numberItems
+                    total += numb * discount.price
+                    remaining = cnt % discount.numberItems
+                    total += self.prices[item] * remaining
+                else:
+                    total += self.prices[item] * cnt
+            else:
+                total += cnt * self.prices[item]
+        return total
+
+    def addDiscount(self, item, numberItems, price):
+        discount = self.Discount(numberItems, price)
+        self.discounts[item] = discount
--- TestCheckout.py
+++ TestCheckout.py
@@ -5,16 +5,26 @@ from Checkout import Checkout
 @pytest.fixture()
 def checkout():
     checkout = Checkout()
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b",2)
     return checkout
 
 def test_canCalculateTotal(checkout):
-    checkout.addItemPrice("a",1)
     checkout.addItem("a")
     assert checkout.calculateTotal() == 1
 
 def test_getCorrectTotalWithMultipleItems(checkout):
-    checkout.addItemPrice("a",1)
-    checkout.addItemPrice("b",2)
     checkout.addItem("a")
     checkout.addItem("b")
     assert checkout.calculateTotal() == 3
+
+def test_canApplyDiscount(checkout):
+    checkout.addDiscount("a",3,2)
+    checkout.addItem("a")
+    checkout.addItem("a")
+    checkout.addItem("a")
+    assert checkout.calculateTotal() == 2
+
+def test_Exception(checkout):
+    with pytest.raises(Exception):
+        checkout.addItem("C")
